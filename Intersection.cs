74a3b30d0014691ef208341e2412c405677d47f4
Sergio Fabián <2021158559@talento.tls.edu.pe>
2025-09-25 13:02:58 -0500

    feat: implement Intersection class - by Sergio Fabian

---
+++ Intersection.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SimulacionTrafico
+{
+    public class Intersection
+    {
+        private string _name;
+        private List<Street> _streets;
+
+        public Intersection(string name)
+        {
+            _name = name;
+            _streets = new List<Street>();
+        }
+
+        public void AddStreet(Street street)
+        {
+            _streets.Add(street);
+        }
+
+        public int GetStreetCount()
+        {
+            return _streets.Count;
+        }
+
+        public string GetName()
+        {
+            return _name;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-using System;
 
 namespace SimulacionTrafico
 {
@@ -15,14 +14,17 @@ namespace SimulacionTrafico
             var car = new Car("ABC123");
             var light = new TrafficLight();
             var street = new Street("Javier Prado");
+            var intersection = new Intersection("Av. Javier Prado x Av. Canadá");
 
             car.Start();
             street.AddCar(car);
             street.AddTrafficLight(light);
+            intersection.AddStreet(street);
 
             Console.WriteLine($"Car {car.GetLicensePlate()} is on {street.GetName()}");
             Console.WriteLine($"There are {street.GetCarCount()} cars on the street.");
             Console.WriteLine($"Traffic light is {light.GetColor()}");
+            Console.WriteLine($"Intersection: {intersection.GetName()} has {intersection.GetStreetCount()} streets.");
 
             Console.ReadKey();
         }
