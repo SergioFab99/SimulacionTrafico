fd89bcb47f807f2507a5553f3be5abe4b6f3ccd9
Sergio Fabián <2021158559@talento.tls.edu.pe>
2025-09-25 10:15:44 -0500

    feat: implement TrafficLight class - by Sergio Fabian

--- Program.cs
+++ Program.cs
@@ -11,10 +11,18 @@ namespace SimulacionTrafico
         static void Main(string[] args)
         {
             var car = new Car("ABC123");
+            var light = new TrafficLight();
+
             car.Start();
             Console.WriteLine($"Car {car.GetLicensePlate()} is moving at {car.GetSpeed()} km/h");
+
+            Console.WriteLine($"Traffic light is {light.GetColor()}");
+            light.ChangeColor();
+            Console.WriteLine($"Traffic light changed to {light.GetColor()}");
+
             car.Stop();
             Console.WriteLine($"Car {car.GetLicensePlate()} stopped.");
+
             Console.ReadKey();
         }
     }
---
+++ TrafficLight.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimulacionTrafico
+{
+    public class TrafficLight
+    {
+        private string _color;
+        private bool _isWorking;
+
+        public TrafficLight()
+        {
+            _color = "Red";
+            _isWorking = true;
+        }
+
+        public void ChangeColor()
+        {
+            switch (_color)
+            {
+                case "Red":
+                    _color = "Green";
+                    break;
+                case "Green":
+                    _color = "Yellow";
+                    break;
+                case "Yellow":
+                    _color = "Red";
+                    break;
+            }
+        }
+
+        public string GetColor()
+        {
+            return _color;
+        }
+
+        public void Break()
+        {
+            _isWorking = false;
+            _color = "Broken";
+        }
+
+        public bool IsWorking()
+        {
+            return _isWorking;
+        }
+    }
+}
